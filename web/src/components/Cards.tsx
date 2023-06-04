51f4295d828695f4fef2d238eca72c042b30e2cf
SantosVicente <santos.vicente@aluno.ifsp.edu.br>
2023-06-04 14:12:40 -0300

    new releases

--- web/src/components/Cards.tsx
+++ web/src/components/Cards.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import {
   Rocket,
   Orbit,
--- web/src/components/ToFind.tsx
+++ web/src/components/ToFind.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import Rocket from "../assets/rocket.svg";
 import Telescope from "../assets/telescope.svg";
 import Galaxy from "../assets/galaxy.svg";
