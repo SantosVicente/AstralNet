e09b3c2967c73104f78c12bbaf0ef57e8b52ef6a
SantosVicente <santos.vicente@aluno.ifsp.edu.br>
2023-06-11 23:44:34 -0300

    new releases

--- web/src/components/AboutUs.tsx
+++ web/src/components/AboutUs.tsx
@@ -1,10 +1,33 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import Profile from '../assets/profile.png';
+
 export default function AboutUs() {
   return (
     <div
       id="about"
-      className="border-t border-zinc-700 flex items-center justify-center h-36"
+      className="border-t border-zinc-700 flex items-center justify-center h-36 overflow-hidden"
     >
       Sobre Nós
+      {/*
+      <div className="flex w-full items-center justify-center gap-4">
+        <div>
+          <Link href="">
+            <p>João Vitor Rodrigues</p>
+          </Link>
+        </div>
+        <div>
+          <Link href="">
+            <p>Mateus Rodrigues Martins</p>
+          </Link>
+        </div>
+        <div>
+          <Link href="" className="flex flex-col gap-2 items-center">
+            <Image src={Profile} alt="" width={70} className="rounded-full" />
+            <p>Vicente dos Santos</p>
+          </Link>
+        </div>
+  </div>*/}
     </div>
   );
 }
--- web/src/components/Cards.tsx
+++ web/src/components/Cards.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 export default function Cards() {
   return (
     <div className="flex flex-col gap-16 items-center">
-      <div className="flex justify-between w-[82vw]">
+      <div className="flex justify-between w-[82vw] px-10">
         <ScrollAnimation delay={0.2} duration={0.9} y={150}>
           <h1 className="font-alt h1 uppercase font-bold w-[40rem]">
             Principais tópicos para você decolar conosco
@@ -15,7 +15,6 @@ export default function Cards() {
         <ScrollAnimationX delay={0.4} duration={0.9} x={150}>
           <div className="flex flex-col gap-6">
             <div className="h-[0.1rem] rounded-md bg-zinc-600 w-20" />
-            {/*Fazer este texto vir com delay e da direita para a esquerda*/}
             <p className="text-lg">
               Estas são algumas de nossas sessões de leitura que você pode
               visitar!
@@ -25,100 +24,72 @@ export default function Cards() {
       </div>
 
       <div className="w-full">
-        <div className="grid grid-cols-3 gap-40 mx-40">
+        <div className="flex gap-28 mx-[12.5rem] justify-center">
           <ScrollAnimation delay={0} duration={0.7} y={100}>
             <Link
               href=""
-              className="border-card1 border border-zinc-600 rounded-md px-16 flex flex-col items-start justify-center h-[30rem] transition-colors text-zinc-400 hover:text-zinc-50 bg-zinc-50 bg-opacity-5"
+              id="card1"
+              className="border-card1 border border-zinc-600 rounded-md px-10 flex flex-col w-[25rem] h-[25rem] transition-colors text-zinc-400 hover:text-zinc-50 bg-zinc-50 bg-opacity-5"
             >
-              <Rocket size={60} />
+              <Rocket size={60} className="mt-[15%]" />
               <div className="mt-4">
-                <h1 className="text-2xl">Introdução à Astronomia</h1>
-                <ul className="ml-6">
-                  <div className="flex gap-1">
-                    <li className="mt-1">O que é Astronomia</li>
-                  </div>
-                  <div className="flex gap-1">
-                    <li className="mt-1">A História da Astronomia</li>
-                  </div>
-                  <div className="flex gap-1">
-                    <li className="mt-1">Instrumentos e Técnicas</li>
-                  </div>
-                  <div className="flex gap-1">
-                    <li className="mt-1">
-                      Conceitos Fundamentais da Astronomia
-                    </li>
-                  </div>
-                  <div className="flex gap-1">
-                    <li className="mt-1">Fenômenos e Eventos Astronômicos</li>
-                  </div>
-                  <div className="flex gap-1">
-                    <li className="mt-1">Cosmologia e o Estudo do Universo</li>
-                  </div>
-                  <div className="flex gap-1">
-                    <li className="mt-1">Aplicações Práticas da Astronomia</li>
-                  </div>
-                </ul>
+                <h1 className="text-3xl font-bold font-alt text-zinc-300">
+                  Introdução à Astronomia
+                </h1>
+
+                <div className="flex gap-1">
+                  <p className="mt-7">
+                    Explore o vasto universo e desvende os segredos do cosmos
+                    com uma introdução envolvente à astronomia. Neste breve
+                    guia, vamos mergulhar nos fundamentos e nas maravilhas que a
+                    astronomia tem a oferecer.
+                  </p>
+                </div>
               </div>
             </Link>
           </ScrollAnimation>
           <ScrollAnimation delay={0.2} duration={0.7} y={100}>
-            <Link href="">
-              <div className="border-card2 border border-zinc-600 rounded-md px-16 flex flex-col items-start justify-center h-[30rem] transition-colors text-zinc-400 hover:text-zinc-50 bg-zinc-50 bg-opacity-5">
-                <Orbit size={60} />
-                <div className="mt-4">
-                  <h1 className="text-2xl">O Universo em Escala</h1>
-                  <ul className="ml-6">
-                    <div className="flex gap-1">
-                      <li className="mt-1">Estrutura do Universo</li>
-                    </div>
-                    <div className="flex gap-1">
-                      <li className="mt-1">Cosmologia Observacional</li>
-                    </div>
-                    <div className="flex gap-1">
-                      <li className="mt-1">Cosmologia Explicada</li>
-                    </div>
-                    <div className="flex gap-1">
-                      <li className="mt-1">Teorias de Formação do Universo</li>
-                    </div>
-                    <div className="flex gap-1">
-                      <li className="mt-1">James Webb e o Efeito Doppler</li>
-                    </div>
-                    <div className="flex gap-1">
-                      <li className="mt-1">
-                        Radiação Cósmica de Fundo em Micro-Ondas
-                      </li>
-                    </div>
-                    <div className="flex gap-1">
-                      <li className="mt-1">Estudos futuros e Desafios</li>
-                    </div>
-                  </ul>
+            <Link
+              href=""
+              id="card2"
+              className="border-card2 border border-zinc-600 rounded-md px-10 flex flex-col w-[25rem] h-[25rem] transition-colors text-zinc-400 hover:text-zinc-50 bg-zinc-50 bg-opacity-5"
+            >
+              <Orbit size={60} className="mt-[15%]" />
+              <div className="mt-4">
+                <h1 className="text-3xl font-bold font-alt text-zinc-300">
+                  O Universo em Escala
+                </h1>
+
+                <div className="flex gap-1">
+                  <p className="mt-7">
+                    Prepare-se para uma jornada extraordinária através das
+                    vastas escalas do universo. Neste guia envolvente,
+                    exploraremos as teorias de formação do universo e
+                    descobriremos as maravilhas da cosmologia observacional.
+                  </p>
                 </div>
               </div>
             </Link>
           </ScrollAnimation>
           <ScrollAnimation delay={0.4} duration={0.7} y={100}>
-            <Link href="">
-              <div className="border-card3 border border-zinc-600 rounded-md px-16 flex flex-col items-start justify-center h-[30rem] transition-colors text-zinc-400 hover:text-zinc-50 bg-zinc-50 bg-opacity-5">
-                <Satellite size={60} />
-                <div className="mt-4">
-                  <h1 className="text-2xl">Explorando o Sistema Solar</h1>
-                  <ul className="ml-6">
-                    <div className="flex gap-1">
-                      <li className="mt-1">Introdução ao Sistema Solar</li>
-                    </div>
-                    <div className="flex gap-1">
-                      <li className="mt-1">Planetas do Sistema Solar</li>
-                    </div>
-                    <div className="flex gap-1">
-                      <li className="mt-1">Exploração de Marte</li>
-                    </div>
-                    <div className="flex gap-1">
-                      <li className="mt-1">
-                        Futuras missões e exploração do Sistema Solar
-                      </li>
-                    </div>
-                  </ul>
+            <Link
+              href=""
+              id="card3"
+              className="border-card3 border border-zinc-600 rounded-md px-10 flex flex-col w-[25rem] h-[25rem] transition-colors text-zinc-400 hover:text-zinc-50 bg-zinc-50 bg-opacity-5"
+            >
+              <Satellite size={70} className="mt-[15%]" />
+              <div className="mt-4">
+                <h1 className="text-3xl font-bold font-alt text-zinc-300">
+                  O Sistema Solar
+                </h1>
+
+                <div className="flex gap-1">
+                  <p className="mt-7">
+                    Entre em uma viagem pelo sistema solar. Neste guia ,
+                    apresentaremos uma introdução fascinante ao nosso sistema
+                    solar, os planetas que o compõem, a exploração de Marte e as
+                    futuras missões que nos aguardam na exploração do espaço.
+                  </p>
                 </div>
               </div>
             </Link>
