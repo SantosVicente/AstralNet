61bd56e44b56690d79eda867aa11bb2729fc19c6
SantosVicente <santos.vicente@aluno.ifsp.edu.br>
2023-07-01 17:40:09 -0300

    teste de deploy no vercel

--- server/src/index.js
+++ server/src/index.js
@@ -5,13 +5,13 @@ const express = require("express");
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
 
 //req = informações de quem faz a requisição (credencial)
