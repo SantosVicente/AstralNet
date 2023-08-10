79c11f35a30695e212266501e0dd6e56917b46e3
Vicente Santos <santos.vicente@aluno.ifsp.edu.br>
2023-08-10 14:14:45 -0300

    Create page.tsx

---
+++ web/src/app/info/introduction/page.tsx
@@ -0,0 +1,7 @@
+export default function Introduction() {
+    return(
+        <div className="pt-16 flex items-center h-screen justify-center">
+            <p>Introduction</p>
+        </div>
+    );
+}
